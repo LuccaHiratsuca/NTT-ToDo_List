77ca71f30d69b4cf0c61f798dbc77fcae2f65f9a
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2024-03-19 09:59:07 -0300

    fix: change name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/Home/home.component';
-import { NotFoundComponent } from './components/Not-found/not-found.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
