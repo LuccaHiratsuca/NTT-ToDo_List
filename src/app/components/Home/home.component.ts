9941a44c0d3c5bc68524ac95ce4daad2871bdeda
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2024-03-19 01:40:13 -0300

    feat: other funtionalities and a better design

--- src/app/components/Home/home.component.ts
+++ src/app/components/Home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { TasksService } from 'src/app/services/tasks.service';
+import { Task } from 'src/app/interfaces/task';
+
 
 @Component({
   selector: 'app-home',
@@ -7,13 +9,56 @@ import { TasksService } from 'src/app/services/tasks.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  newTask: Task = { title: '', description: '', done: false };
+  editableTask: Task = { title: '', description: '', done: false };
+  editingIndex: number = -1; // To track which task is being edited
 
-  constructor(public tasksService: TasksService) { }
+  // Access the modal element
+  @ViewChild('updateModal') updateModal!: ElementRef;
+
+
+  constructor(public tasksService: TasksService, private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
-  deleteTask(index: number){
-    this.tasksService.deleteTask(index);
+  addTask() {
+    if (!this.newTask.title) return;
+    this.tasksService.addTask({ ...this.newTask });
+    this.newTask = { title: '', description: '', done: false }; // Reset new task input
   }
+
+  openUpdateModal(task: Task, index: number) {
+    this.editableTask = { ...task };
+    this.editingIndex = index;
+    this.showModal();
+  }
+
+
+  toggleDone(index: number) {
+    this.tasksService.toggleDone(index);
+  }
+
+  updateTask() {
+    if (this.editingIndex !== -1) {
+      this.tasksService.updateTask(this.editingIndex, this.editableTask);
+      this.hideModal();
+    }
+  }
+
+  private showModal() {
+    this.renderer.addClass(this.updateModal.nativeElement, 'show');
+    this.renderer.setStyle(this.updateModal.nativeElement, 'display', 'block');
+    this.renderer.setAttribute(this.updateModal.nativeElement, 'aria-hidden', 'false');
+    this.renderer.setAttribute(this.updateModal.nativeElement, 'aria-modal', 'true');
+    this.renderer.setStyle(document.body, 'overflow', 'hidden');
+  }
+
+  private hideModal() {
+    this.renderer.removeClass(this.updateModal.nativeElement, 'show');
+    this.renderer.removeStyle(this.updateModal.nativeElement, 'display');
+    this.renderer.setAttribute(this.updateModal.nativeElement, 'aria-hidden', 'true');
+    this.renderer.removeStyle(document.body, 'overflow');
+  }
+
 }
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -5,25 +5,39 @@ import { Task } from '../interfaces/task';
   providedIn: 'root'
 })
 export class TasksService {
-
   tasks: Task[] = [
     {
       title: 'First Task',
-      description: 'This is the first task'
+      description: 'This is the first task',
+      done: false
     },
     {
       title: 'Second Task',
-      description: 'This is the second task'
+      description: 'This is the second task',
+      done: false
     },
     {
       title: 'Third Task',
-      description: 'This is the third task'
+      description: 'This is the third task',
+      done: false
     },
   ];
+
   constructor() { }
 
-  deleteTask(index: number){
+  addTask(task: Task) {
+    this.tasks.push(task);
+  }
+
+  deleteTask(index: number) {
     this.tasks.splice(index, 1);
   }
-  
+
+  updateTask(index: number, task: Task) {
+    this.tasks[index] = task;
+  }
+
+  toggleDone(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+  }
 }
